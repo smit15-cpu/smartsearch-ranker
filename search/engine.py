530ca9470060f3ac4794db4feb5dafad8eb84581
Smit Anghan <smitanghan.15@gmail.com>
2026-05-09 21:41:22 +0100

    implemented  tfidf based search ranking

--- search/engine.py
+++ search/engine.py
@@ -1,22 +1,35 @@
 from data.documents import documents
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+doc_texts = [
+    doc["title"] + " " + doc["content"]
+    for doc in documents
+]
+
+vectorizer = TfidfVectorizer(stop_words="english")
+
+document_vectors = vectorizer.fit_transform(doc_texts)
 
 
 def search(query):
-    query_words = query.lower().split()
+    query_vector = vectorizer.transform([query])
+
+    similarities = cosine_similarity(
+        query_vector,
+        document_vectors
+    ).flatten()
 
     ranked_results = []
 
-    for doc in documents:
-        score = 0
-
-        text = (doc["title"] + " " + doc["content"]).lower()
-
-        for word in query_words:
-            score += text.count(word)
-
+    for index, score in enumerate(similarities):
         if score > 0:
-            ranked_results.append((score, doc))
+            ranked_results.append((score, documents[index]))
 
-    ranked_results.sort(reverse=True, key=lambda x: x[0])
+    ranked_results.sort(
+        reverse=True,
+        key=lambda x: x[0]
+    )
 
     return ranked_results
